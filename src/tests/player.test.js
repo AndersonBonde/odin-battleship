491a420ac0713c0131a4cb0cfdcd5a3d49d21c91
Anderson <andersonbonde@gmail.com>
2023-12-19 12:09:15 -0300

    Remove superfluous test

--- src/tests/player.test.js
+++ src/tests/player.test.js
@@ -4,14 +4,15 @@ import events from '../events';
 // Mock the events module;
 jest.mock('../events');
 
-test('New player is defined', () => {
-  const player1 = player();
-  expect(player1).toBeDefined();
-});
-
 test('Initialize turn correctly', () => {
   const player1 = player();
   expect(player1.turn).toBe(1);
+});
+
+test('Should increase turn after attack is called', () => {
+  const player1 = player();
+  player1.attack(2, 3);
+  expect(player1.turn).toBe(2);
 });
 
 test('Call the events.emit with the correct params', () => {
