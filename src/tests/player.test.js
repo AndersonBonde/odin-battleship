4158501a9dd0dc1e3e7a2e018b0c22b2513a6c99
Anderson <andersonbonde@gmail.com>
2024-01-30 14:20:09 -0300

    Add player.test

--- src/tests/player.test.js
+++ src/tests/player.test.js
@@ -1,11 +1,25 @@
 const player = require('../factories/player');
 
+const mockMyBoard = {
+  receiveRandomAttack: jest.fn(),
+};
+
+const mockEnemyBoard = {
+  receiveAttack: jest.fn(),
+};
+
 let myPlayer;
 
 beforeEach(() => {
-  myPlayer = player();
+  myPlayer = player(mockMyBoard, mockEnemyBoard);
 });
 
-test('First', () => {
+test('Player attack will call receiveAttack on the enemy board', () => {
+  myPlayer.attack(1, 2);
+  expect(mockEnemyBoard.receiveAttack).toHaveBeenCalledWith(1, 2);
+});
 
+test('Player attack will call receiveRandomAttack on the myBoard', () => {
+  myPlayer.attack(2, 5);
+  expect(mockMyBoard.receiveRandomAttack).toHaveBeenCalled();
 });
